4cb0632b661739a64914a3c372546a802ecc9dfb
Shweta-Prasad <143308786+Shweta-Prasad@users.noreply.github.com>
2024-05-08 21:41:19 +0530

    Add files via upload

--- boundaryfill.cpp
+++ boundaryfill.cpp
@@ -5,10 +5,14 @@
 #include <time.h>
 */
 
-const int WINDOW_WIDTH = 640;
-const int WINDOW_HEIGHT = 480;
+void init() {
+    glClearColor(1.0, 1.0, 1.0, 0.0);
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(0, 640, 0, 480);
+}
 
-void boundFill(int x, int y, float *fillColor, float *boundaryColor) {
+
+void boundFill(int x, int y, float fillColor[3], float boundaryColor[3]) {
     float color[3];
     glReadPixels(x, y, 1.0, 1.0, GL_RGB, GL_FLOAT, color);
     if ((color[0] != boundaryColor[0] || color[1] != boundaryColor[1] || color[2] != boundaryColor[2]) &&
@@ -19,15 +23,15 @@ void boundFill(int x, int y, float *fillColor, float *boundaryColor) {
         glEnd();
         glFlush();
         boundFill(x + 1, y, fillColor, boundaryColor);
-        boundFill(x - 1, y, fillColor, boundaryColor);
-        boundFill(x, y + 1, fillColor, boundaryColor);
-        boundFill(x, y - 1, fillColor, boundaryColor);
+        boundFill(x - 2, y, fillColor, boundaryColor);
+        boundFill(x, y + 2, fillColor, boundaryColor);
+        boundFill(x, y - 2, fillColor, boundaryColor);
     }
 }
 
 void mouse(int button, int state, int x, int y) {
     if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-        y = WINDOW_HEIGHT - y;
+        y = 480 - y;
         float fillColor[] = {0, 0, 1}; // Blue
         float boundaryColor[] = {1, 0, 0}; // Red
         boundFill(x, y, fillColor, boundaryColor);
@@ -37,6 +41,7 @@ void mouse(int button, int state, int x, int y) {
 void drawShape() {
     glLineWidth(3);
     glPointSize(2);
+
     glClear(GL_COLOR_BUFFER_BIT);
     glColor3f(1, 0, 0); // Red
     glBegin(GL_LINE_LOOP);
@@ -44,18 +49,14 @@ void drawShape() {
     glVertex2i(300, 300);
     glVertex2i(450, 100);
     glEnd();
+    
     glFlush();
-}
-
-void init() {
-    glClearColor(1.0, 1.0, 1.0, 0.0); // White background
-    gluOrtho2D(0, WINDOW_WIDTH, 0, WINDOW_HEIGHT);
 }
 
 int main(int argc, char **argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    glutInitWindowSize(640, 480);
     glutInitWindowPosition(200, 200);
     glutCreateWindow("Boundary Fill");
     glutDisplayFunc(drawShape);
